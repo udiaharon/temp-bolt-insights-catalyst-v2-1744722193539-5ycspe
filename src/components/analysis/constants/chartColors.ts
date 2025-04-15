8ba1cec514fe8f9bba1c3c596a67c18328e22270
Udi Aharon <udi.aharon@pursway.com>
2025-04-15 16:05:33 +0300

    Copy src/components/analysis/constants/chartColors.ts from bolt-insights-catalyst-v2

---
+++ src/components/analysis/constants/chartColors.ts
@@ -0,0 +1,12 @@
+
+export const COMPETITOR_COLORS = [
+  "#3E66FB", // Main brand - theme2 primary blue
+  "#95B8E5", // Darker pastel blue
+  "#FF9EA8", // Darker pastel pink
+  "#B5ACF2", // Darker pastel purple
+  "#88D7B0", // Darker pastel green
+  "#F7C56B", // Darker pastel yellow
+  "#F0A0CC", // Darker pastel rose
+  "#92BBF3", // Another darker pastel blue
+  "#C7C7C4", // Darker pastel gray
+];
