c514d211469e381fb0500641e44442a6e21f3f8f
Udi Aharon <udi.aharon@pursway.com>
2025-04-15 16:09:13 +0300

    Copy src/utils/perplexity/trends/index.ts from bolt-insights-catalyst-v2

---
+++ src/utils/perplexity/trends/index.ts
@@ -0,0 +1,5 @@
+
+export { parseTrendsResponse } from "./trendParser";
+export { parseCategoryInfo, type CategoryInfo } from "./categoryParser";
+export { processLatestTrendsCitations } from "./citationProcessor";
+export { generateTrendsSystemPrompt, generateTrendsUserPrompt } from "./trendPrompts";
