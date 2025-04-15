04e8fbaabe61152d174d2e30747b77b265323feb
Udi Aharon <udi.aharon@pursway.com>
2025-04-15 16:08:03 +0300

    Copy src/hooks/use-brand-content.ts from bolt-insights-catalyst-v2

---
+++ src/hooks/use-brand-content.ts
@@ -0,0 +1,6 @@
+
+export { useBrandContent } from './brand-content/use-brand-content';
+
+// Also re-export the cache clear functions for direct access
+export { clearBrandContentCache } from './brand-content/fetch-brand-content';
+export { clearNewsCache } from './brand-content/fetch-news-items';
